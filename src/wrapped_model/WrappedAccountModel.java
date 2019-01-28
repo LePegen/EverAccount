553c17b3880710756e58df36564884550272d6eb
Gene Garcia <genejogarcia.gg@gmail.com>
2019-01-28 16:29:07 +0800

    added wrappedacountmodel class and package

---
+++ src/wrapped_model/WrappedAccountModel.java
@@ -0,0 +1,15 @@
+package wrapped_model;
+
+import view.View;
+
+/**
+ *
+ * @author Gene Garcia
+ */
+public class WrappedAccountModel {
+    
+    public void pushToDB(View v){
+        
+    }
+    
+}
