897b4e1c250ad4a9afb98dc24286586a7ff6c377
PRG08P-A67-13 <PRG08P-A67-13@R214-13-D.mcl.edu.ph.local>
2019-01-30 08:38:18 +0800

    modified AccountOverviewDataObject

--- src/database/AccountOverviewDataObject.java
+++ src/database/AccountOverviewDataObject.java
@@ -5,16 +5,43 @@
  */
 package database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.AccountModel;
+
 /**
  *
  * @author lipat
  */
-public class AccountOverviewDataObject {
-    
-    private DBConnection connection;
-    
-    public AccountOverviewDataObject(DBConnection connection){
-        this.connection = connection;
+public class AccountOverviewDataObject extends DataObject{
+
+    public AccountOverviewDataObject(DBConnection connection) {
+        super(connection);
     }
-    
+
+    public ArrayList<AccountModel> getAccount(int userID) throws SQLException {
+
+        String query = String.format("SELECT * FROM ACCOUNTS() WHERE ACCOUNT_NAME  = '%d'", userID);
+        this.connection.executeCommand(query);
+        ResultSet set = this.connection.getData();
+        ArrayList<AccountModel> model = new ArrayList<>();
+        
+        while (set.next()) {
+            AccountModel tempModel = new AccountModel();
+            
+            tempModel.setAccountID( set.getInt("ACCOUNT_ID") );
+            tempModel.setUniqueName(set.getString("UNIQUE_NAME") );
+            tempModel.setEmail(set.getString("EMAIL") );
+            tempModel.setUsername(set.getString("USERNAME") );
+            tempModel.setPassword(set.getString("PASSWORD") );
+            tempModel.setAdditionalInformation(set.getString("ADDITIONAL_INFORMATION") );
+            
+            model.add(tempModel);
+        }
+
+        return model;
+
+    }
+
 }
