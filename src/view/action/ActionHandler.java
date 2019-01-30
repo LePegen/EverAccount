2170d4ef3a8af2b3922d99329eb4cd3d6df02dfb
Gene Garcia <genejogarcia.gg@gmail.com>
2019-01-30 18:24:36 +0800

    Controller must be initialized from Controller

--- src/view/action/ActionHandler.java
+++ src/view/action/ActionHandler.java
@@ -3,11 +3,14 @@ package view.action;
 import controller.Controller;
 
 public abstract class ActionHandler {
-    protected Controller controller;
-    
-    
-    //Masterclass
 
+    protected Controller controller;
+
+    //to do: Controller must be initialized
+    public ActionHandler(){
+        
+    }
+    
     public Controller getController() {
         return controller;
     }
